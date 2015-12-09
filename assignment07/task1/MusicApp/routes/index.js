f1a894ae728510741eb1269c9de5a5977469831a
bAckmumu <bAckmumu@gmail.com>
2015-12-09 18:11:08 +0100

    Assignment07 completed task3

--- assignment07/task1/MusicApp/routes/index.js
+++ assignment07/task1/MusicApp/routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+
+
+var spotify = require('./spotify');
+
 var router = express.Router();
+
+router.use('/spotify', spotify);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- assignment07/task1/MusicApp/routes/spotify.js
+++ assignment07/task1/MusicApp/routes/spotify.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path');
+
 var router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../spotifysearch')));
