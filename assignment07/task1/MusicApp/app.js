44a7331cbae8f30b293bc106434853cfc271f42a
bAckmumu <bAckmumu@gmail.com>
2015-12-15 10:06:36 +0100

    Assignment07 implementing task4

--- assignment07/task1/MusicApp/app.js
+++ assignment07/task1/MusicApp/app.js
@@ -8,6 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var VisitCounter = require('./customModules/VisitCounter');
+
+var visitCounter = new VisitCounter();
+
 var app = express();
 
 // view engine setup
@@ -16,6 +20,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(visCounter.countMiddleware);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/showVisit', function(req, res) {
+  res.json(visCounter.getCount);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- assignment07/task1/MusicApp/routes/index.js
+++ assignment07/task1/MusicApp/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-
+var router = express.Router();
 
 var spotify = require('./spotify');
 
-var router = express.Router();
 
 router.use('/spotify', spotify);
 
